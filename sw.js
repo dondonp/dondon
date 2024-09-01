fbaac9c2a7ca9e18ded98c10bfb3190898c5f97d
dondonp <84660604+dondonp@users.noreply.github.com>
2024-09-01 15:25:38 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,28 +1,32 @@
-var CACHE_NAME = 'color-name';
-var urlsToCache = [
-    '/color-name/js/main.js',
-    '/color-name/css/normalize.css',
-    '/color-name/css/common.css'
+const CACHE_NAME = 'bearing-inspection-app-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192x192.png',
+  '/icon-512x512.png',
+  'https://sdk.amazonaws.com/js/aws-sdk-2.1386.0.min.js'
 ];
 
-// インストール処理
 self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches
-            .open(CACHE_NAME)
-            .then(function(cache) {
-                return cache.addAll(urlsToCache);
-            })
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
-// リソースフェッチ時のキャッシュロード処理
 self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches
-            .match(event.request)
-            .then(function(response) {
-                return response || fetch(event.request);
-            })
-    );
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
 });
